cd0d466a84ae54853e00158cd430fda2dccd54b0
Rodrigo Moraes <rodrigo_moraes@intuit.com>
2025-06-05 18:12:27 -0300

    implement find category by course id

--- internal/database/category.go
+++ internal/database/category.go
@@ -45,3 +45,13 @@ func (c *Category) FindAll() ([]Category, error) {
 
 	return categories, nil
 }
+
+func (c *Category) FindByCourseID(CourseID string) (*Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("SELECT categories.id, categories.name, categories.description FROM categories INNER JOIN courses ON categories.id = courses.category_id WHERE courses.id = $1", CourseID).Scan(&id, &name, &description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Category{ID: id, Name: name, Description: description}, nil
+}
